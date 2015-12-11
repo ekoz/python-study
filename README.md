fb1332404f7c1314f37cf82bb380b3a499b6470e
ekoz <eko.z@hotmail.com>
2015-12-11 13:53:58 +0800

    - openoffice.sh，用法：./openoffice.sh start ./openoffice.sh stop

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ###	openoffice.py
 * 采用OpenOffice附件转换时，由于一些无法估测的因素转换失败，导致OpenOffice进程卡死，每隔五分钟检查一次openoffice端口，如果无法ping通，则启动openoffice
+* openoffice.sh 启动命令 ./openoffice.sh start 停止命令 ./openoffice.sh stop
 
 ### rmfile.py
 * 定时清理 Jetty 服务器产生的日志
--- openoffice.sh
+++ openoffice.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 #author eko.zhan
 
-#export DISPLAY=:0.0
-#export DISPLAY=localhost:1
-soffice --headless --accept="socket,host=127.0.0.1,port=8100;urp;" --nofirststartwizard &
+start(){
+	#export DISPLAY=:0.0
+	export DISPLAY=localhost:1
+	soffice --headless --accept="socket,host=127.0.0.1,port=8100;urp;" --nofirststartwizard &
+	return 0
+}
+
+stop(){
+	pkill soffice
+	#kill -9 $(ps -ef|grep soffice|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+	return 0
+}
+
+case $1 in 
+	start)
+		start
+	;;
+	stop)
+		stop
+	;;
+	*)
+		echo "Usage: {start|stop}"
+	;;
+esac
