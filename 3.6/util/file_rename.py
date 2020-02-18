a3abd70e41428b5a73bb3bed393861e731a91acb
eko.zhan <eko.z@outlook.com>
2020-02-18 09:46:45 +0800

    feat: file_rename.py add VID_

--- 3.6/util/file_rename.py
+++ 3.6/util/file_rename.py
@@ -33,8 +33,9 @@ def walk(path_list):
                 elif f.find('20') == 0:
                     # 文件名称是已 2019 或 2020 年月日开头，不作处理
                     continue
-                elif f.find('IMG_') == 0:
+                elif f.find('IMG_') == 0 or f.find('VID_') == 0:
                     # 手机拍照以 IMG_ 开头
+                    # 视频文件名以 VID_ 开头
                     new_name = f[4:]
                     old_path = path + '\\' + f
                     new_path = path + '\\' + new_name
