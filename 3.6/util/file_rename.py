1c7df65a8627999bff48214e5a982951ac2dac80
eko.zhan <eko.z@outlook.com>
2020-09-24 23:49:20 +0800

    feat: file_rename.py add Screenshot_

--- 3.6/util/file_rename.py
+++ 3.6/util/file_rename.py
@@ -24,38 +24,51 @@ def walk(path_list):
         # print(path)
         fs = os.listdir(path)
         for f in fs:
-            if f.find('-') == -1:
+            if f.find("-") == -1:
                 # 文件名以 mmexport 开头
-                if f.find('mmexport') == 0:
+                if f.find("mmexport") == 0:
                     # 微信保存的图片已 mmexport 开头
-                    dt = time.localtime(
-                        int(f[8:].split('.')[0]) / 1000)
-                elif f.find('20') == 0:
+                    dt = time.localtime(int(f[8:].split(".")[0]) / 1000)
+                elif f.find("20") == 0:
                     # 文件名称是已 2019 或 2020 年月日开头，不作处理
                     continue
-                elif f.find('IMG_') == 0 or f.find('VID_') == 0:
+                elif f.find("IMG_") == 0 or f.find("VID_") == 0:
                     # 手机拍照以 IMG_ 开头
                     # 视频文件名以 VID_ 开头
                     new_name = f[4:]
-                    old_path = path + '\\' + f
-                    new_path = path + '\\' + new_name
+                    old_path = path + "\\" + f
+                    new_path = path + "\\" + new_name
                     os.rename(old_path, new_path)
+                    continue
+                elif len(f) > 16:
+                    # 文件名不是时间戳
                     continue
                 else:
                     # 时间戳文件名
-                    dt = time.localtime(int(f.split('.')[0]) / 1000)
-                dtstr = time.strftime('%Y-%m-%d_%H.%M.%S', dt)
-                new_name = dtstr + '.' + f.split('.')[1]
+                    dt = time.localtime(int(f.split(".")[0]) / 1000)
+                dtstr = time.strftime("%Y-%m-%d_%H.%M.%S", dt)
+                new_name = dtstr + "." + f.split(".")[1]
 
-                old_path = path + '\\' + f
-                new_path = path + '\\' + new_name
+                old_path = path + "\\" + f
+                new_path = path + "\\" + new_name
                 if os.path.exists(new_path):
                     i = i + 1
-                    new_path = path + '\\' + dtstr + \
-                        str(i) + '.' + f.split('.')[1]
+                    new_path = path + "\\" + dtstr + str(i) + "." + f.split(".")[1]
                 os.rename(old_path, new_path)
+            elif f.find("Screenshot_") == 0:
+                # 手机截屏文件名前缀
+                fmt_prefix(path, f, "Screenshot_")
+                continue
     else:
-        print(path + ' is not directory.')
+        print(path + " is not directory.")
+
+
+def fmt_prefix(path, file_name, prefix):
+    # 根据前缀格式化文件名，移除前缀，保留后面的文本
+    new_name = file_name[len(prefix) :]
+    old_path = path + "\\" + file_name
+    new_path = path + "\\" + new_name
+    os.rename(old_path, new_path)
 
 
 class Main:
@@ -63,5 +76,5 @@ class Main:
         pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     walk(sys.argv[1:])
