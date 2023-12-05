c9059faa3faeb0211c712b3cdcf705bcfcde9896
eko.zhan <eko.z@outlook.com>
2023-12-05 17:46:31 +0800

    feat: 增加 PaddleOCR 用例

---
+++ PaddleOCR/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @author   :   eko.zhan
+# @time     :   2023/12/5 13:49
+# https://github.com/PaddlePaddle/PaddleOCR
+
+from paddleocr import PaddleOCR, draw_ocr
+
+# Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
+# 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
+ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
+img_path = './ppocr_img/imgs/202312051510.png'
+result = ocr.ocr(img_path, cls=True)
+for idx in range(len(result)):
+    res = result[idx]
+    for line in res:
+        print(line)
+
+# 显示结果
+from PIL import Image
+result = result[0]
+image = Image.open(img_path).convert('RGB')
+boxes = [line[0] for line in result]
+txts = [line[1][0] for line in result]
+scores = [line[1][1] for line in result]
+im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
+im_show = Image.fromarray(im_show)
+im_show.save('result.jpg')
+
