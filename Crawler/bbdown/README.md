d604521d6931fa907db681a3dfe58dbecc65fd08
eko.zhan <eko.z@outlook.com>
2024-08-02 17:57:37 +0800

    feat: bbdown 读取配置

--- Crawler/bbdown/README.md
+++ Crawler/bbdown/README.md
@@ -1,9 +1,10 @@
-# B站音频下载器
+# B 站音频下载器
+
 > 支持下载合集中的所有音频文件以及单个视频中的音频文件
 
 ## 说明
+
 主要是采用 BBDown 进行下载 https://github.com/nilaoda/BBDown
 ，下载 bbdown.exe 到当前目录中
 
-手动修改 main.py 中的 cookie，以及指定待下载的B站链接
-
+手动修改 config.ini 中的 cookie，以及指定待下载的 B 站链接、BBDown.exe 路径
--- Crawler/bbdown/main.py
+++ Crawler/bbdown/main.py
@@ -6,18 +6,23 @@ from util.avid import av2bv, bv2av
 import requests
 import os
 import subprocess
+import configparser
 
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
-robot_cookie = "请填入cookie"
+config = configparser.RawConfigParser()
+config.read("config.ini", encoding="utf-8")
+ddown_path = config["DEFAULT"]["ddown_path"]
+
+robot_cookie = config["DEFAULT"]["cookie"]
+
+# 请输入你要下载的b站视频的 bvid，即 https://www.bilibili.com/video/BV1nx4y1471s/ 中的 BV1nx4y1471s 这部分
+bvid = config["DEFAULT"]["bvid"]
 
 headers = {
     "Cookie": robot_cookie,
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
 }
-
-# 请输入你要下载的b站视频的 bvid，即 https://www.bilibili.com/video/BV1nx4y1471s/ 中的 BV1nx4y1471s 这部分
-bvid = "BV19E4m1X7jD"
 
 avid = bv2av(bvid)
 
@@ -35,7 +40,7 @@ resp = requests.get(
 if resp.json()["data"]["View"].get("ugc_season") is None:
     # 该链接是单个视频，下载该文件即可
     url_video = f"https://www.bilibili.com/video/{bvid}"
-    return_code = subprocess.call(["BBDown.exe", url_video, "--audio-only"], shell=True)
+    return_code = subprocess.call([ddown_path, url_video, "--audio-only"], shell=True)
     print(f"下载 {url_video}，获取结果 {return_code}")
 else:
     # 该链接存在合集，直接下载合集
@@ -50,6 +55,7 @@ else:
             t_bvid = episode["bvid"]
             url_video = f"https://www.bilibili.com/video/{t_bvid}"
             return_code = subprocess.call(
-                ["BBDown.exe", url_video, "--audio-only"], shell=True
+                [ddown_path, url_video, "--audio-only", "--work-dir", sections_title],
+                shell=True,
             )
             print(f"下载 {url_video}，获取结果 {return_code}")
