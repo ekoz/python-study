84cf5be5031900a48cf478a96419fc0b92e1966d
eko.zhan <eko.z@outlook.com>
2024-11-27 21:55:19 +0800

    feat: batch handle

---
+++ ModelScope/snapshot_download.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @author   :   eko.zhan
+# @time     :   2024/11/21 14:25
+# SDK模型下载
+from modelscope import snapshot_download
+
+model_dir = snapshot_download('damo/cv_resnet50_face-detection_retinaface')
